8c5993a4cdb84e44d4e402e9f472fd1e9bf3a987
MemberJR <179604553+ascomlexicon@users.noreply.github.com>
2025-02-09 21:04:56 +0000

    Create basic skeleton for the Spring backend

---
+++ backend/src/main/java/com/model/junction/JunctionController.java
@@ -0,0 +1,13 @@
+package com.model.junction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JunctionController {
+  @GetMapping("/hello")
+  public String sayHello() {
+      return "Hello, World!";
+  }
+}
+
---
+++ backend/src/test/java/com/model/junction/JunctionApplicationTests.java
@@ -0,0 +1,12 @@
+package com.model.junction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunctionApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
+
