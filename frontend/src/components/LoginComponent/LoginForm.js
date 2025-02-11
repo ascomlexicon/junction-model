54bbaed82e9db7e44ac1eee3da889eddbf7eb6b4
Joshua Kassie <kassiejoshua57@gmail.com>
2025-02-11 18:00:21 +0000

    fixed login page

--- frontend/src/components/LoginComponent/LoginForm.js
+++ frontend/src/components/LoginComponent/LoginForm.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './LoginForm.module.css';
 
 
-function LoginForm(){
+function LoginForm() {
     return (
-        <form>
-            <input type="text" placeholder="Enter Email" name="Email" required/>
-            <input type="password" placeholder="Enter Password" name="Password" required/>
-            <button><Link to = "/MainPage"> Login </Link></button>
-        </form>
-    )
+        <div className={styles.container}>
+            <div className={styles.Title}>
+                <h1>Junction Simulator</h1>
+            </div>
+            <div className={styles.Form}>
+                <form>
+                    <div className={styles.input}>
+                        <label>Username:</label>
+                        <input type="text" name="Email" required/>
+                    </div>
+                    <div className={styles.input}>
+                        <label>Password:</label>
+                        <input type="password" name="Password" required/>
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <button>
+                            <Link to="/MainPage">Login</Link>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default LoginForm;
