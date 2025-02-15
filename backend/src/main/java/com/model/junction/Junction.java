02deeb03a6dc2e26ead1630f5e3f10f0c435fc95
JR <179604553+ascomlexicon@users.noreply.github.com>
2025-02-15 22:51:42 +0000

    Create basic implementation for the Junction class

--- backend/src/main/java/com/model/junction/Junction.java
+++ backend/src/main/java/com/model/junction/Junction.java
@@ -1,5 +1,26 @@
 package com.model.junction;
 
-public class Junction {
+
+public class Junction implements Comparable<Junction> {
+  // Attributes
+  private String name;
+
+  // Constructors
+  public Junction(String junctionName) {
+    this.name = junctionName;
+  }
   
+  // Accessors and Mutators
+  public String getName() {
+    return name;
+  }
+
+   public void setName(String name) {
+    this.name = name;
+  } 
+  
+  // Implementations
+  public int compareTo(Junction other) {
+    return this.name.compareTo(other.getName()); 
+  }
 }
