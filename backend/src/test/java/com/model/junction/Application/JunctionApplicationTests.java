f6f8dd1f039f57bef350ddbf1b32cba4b67d5a6d
JR <179604553+ascomlexicon@users.noreply.github.com>
2025-02-20 13:23:02 +0000

    Restructure the project directory structure

--- backend/src/test/java/com/model/junction/Application/JunctionApplicationTests.java
+++ backend/src/test/java/com/model/junction/Application/JunctionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.model.junction;
+package com.model.junction.Application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- backend/src/test/java/com/model/junction/JunctionClasses/JunctionStorageTest.java
+++ backend/src/test/java/com/model/junction/JunctionClasses/JunctionStorageTest.java
@@ -1,6 +1,7 @@
-package com.model.junction;
+package com.model.junction.JunctionClasses;
 
 import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
