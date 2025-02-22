8224f46a179598ec8b63dd3965230d5859205583
JR <179604553+ascomlexicon@users.noreply.github.com>
2025-02-22 20:37:04 +0000

    Create a template for Projects

--- backend/src/main/java/com/model/junction/ProjectClasses/Project.java
+++ backend/src/main/java/com/model/junction/ProjectClasses/Project.java
@@ -2,11 +2,37 @@ package com.model.junction.ProjectClasses;
 
 import java.util.HashMap;
 
-import com.model.junction.Direction;
-import com.model.junction.JunctionStorage;
+import com.model.junction.Attributes.Direction;
+import com.model.junction.JunctionClasses.JunctionStorage;
 
-public class Project {
+public class Project implements Comparable<Project> {
   private HashMap<Direction, HashMap<Direction, Integer>> vehiclePerHourData;
   private JunctionStorage junctions; 
-  private String name;
+  private String projectTitle;
+
+  // Constructors
+  public Project(String title) {
+    this.vehiclePerHourData = new HashMap<Direction, HashMap<Direction, Integer>>();
+    this.junctions = new JunctionStorage();
+    setName(title);
+  }
+  
+  // Accessors and Mutators
+  public String getProjectTitle() {
+    return this.projectTitle;
+  }
+
+  public void setName(String newTitle) {
+    this.projectTitle = newTitle;
+  }
+
+  // Data Pipeline Methods
+  private HashMap<Direction, HashMap<Direction, Integer>> vehicleJSONToHashMap(String dataJSON) {
+    return new HashMap<>();
+  }
+
+  // Comparators
+  public int compareTo(Project other) {
+    return 0;
+  }
 }
