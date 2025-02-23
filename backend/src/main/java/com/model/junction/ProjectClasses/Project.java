914ae28db572be3c3d48030c66c113ff311eeda7
JR <179604553+ascomlexicon@users.noreply.github.com>
2025-02-23 01:12:35 +0000

    Add comparators for Projects

--- backend/src/main/java/com/model/junction/ProjectClasses/Project.java
+++ backend/src/main/java/com/model/junction/ProjectClasses/Project.java
@@ -5,16 +5,15 @@ import java.util.HashMap;
 import com.model.junction.Attributes.Direction;
 import com.model.junction.JunctionClasses.JunctionStorage;
 
-public class Project implements Comparable<Project> {
+public class Project {
   private HashMap<Direction, HashMap<Direction, Integer>> vehiclePerHourData;
   private JunctionStorage junctions; 
   private String projectTitle;
 
   // Constructors
   public Project(String title) {
-    this.vehiclePerHourData = new HashMap<Direction, HashMap<Direction, Integer>>();
+    setProjectTitle(title);
     this.junctions = new JunctionStorage();
-    setName(title);
   }
   
   // Accessors and Mutators
@@ -22,17 +21,57 @@ public class Project implements Comparable<Project> {
     return this.projectTitle;
   }
 
-  public void setName(String newTitle) {
+  public void setProjectTitle(String newTitle) {
     this.projectTitle = newTitle;
   }
+  
+  public HashMap<Direction, HashMap<Direction, Integer>> getVehichlePerHourData() {
+    return this.vehiclePerHourData;
+  }
 
-  // Data Pipeline Methods
-  private HashMap<Direction, HashMap<Direction, Integer>> vehicleJSONToHashMap(String dataJSON) {
-    return new HashMap<>();
+  public boolean setVehiclePerHourData(HashMap<Direction, HashMap<Direction, Integer>> vphData) {
+    // Once it is first set, then the VPH cannot be changed.
+    if (this.vehiclePerHourData == null) {
+      this.vehiclePerHourData = vphData;
+      return true;
+    }
+    
+    return false;
   }
 
   // Comparators
-  public int compareTo(Project other) {
-    return 0;
+  public boolean equalVPHData(Project other) {
+    int equalityCounter = 0;
+    HashMap<Direction, HashMap<Direction, Integer>> thisHashMap = getVehichlePerHourData();
+    HashMap<Direction, HashMap<Direction, Integer>> otherHashMap = other.getVehichlePerHourData();
+
+    for (Direction direction : Direction.values()) {
+      HashMap<Direction, Integer> thisEntry = thisHashMap.get(direction);
+      HashMap<Direction, Integer> otherEntry = otherHashMap.get(direction);
+      
+      if (thisEntry.equals(otherEntry)) {
+        equalityCounter++;
+      }
+    }
+    
+    return equalityCounter == 4;
+  }
+  
+  @Override
+  public boolean equals(Object other) {
+    if (other == this) {
+      return true;
+    }
+    if (other == null) {
+      return false;
+    }
+    if (getClass() != other.getClass()) {
+      return false;
+    }
+    
+    Project project = (Project) other;
+
+    // Projects are equal if they share the same name or VPH data.
+    return equalVPHData(project) || getProjectTitle().equals(project.getProjectTitle());
   }
 }
