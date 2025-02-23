e04ba4fae6de840a993e4e4a4966a05c331c50f1
Luke Szappanyos <szappy99@gmail.com>
2025-02-23 09:24:23 +0000

    Create logic for prioritised traffic flow

--- backend/src/main/java/com/model/junction/JunctionClasses/JunctionSimulationLogic.java
+++ backend/src/main/java/com/model/junction/JunctionClasses/JunctionSimulationLogic.java
@@ -27,6 +27,69 @@ class JunctionSimulationLogic {
     int exitingEast = 150; // cars exiting east
     int exitingWest = 100; // cars exiting west
 
+    // PRIORITIESED TRAFFIC FLOW: To implement this, we need to give each of the lanes a priority from 0-4 and this will determine the green light
+    // length for each lane. Below are the times (time is done in real life seconds, not simulation seconds)
+    /* 0 -> 5 seconds
+     * 1 -> 10 seconds
+     * 2 -> 20 seconds
+     * 3 -> 30 seconds
+     * 4 -> 45 seconds
+     */
+    // On top of that, the priority will determine the initial sequencing order for the traffic lights so for example if the current arrangement
+    // of priorities is North 1, East 0, South 4, West 2, then the order will be South, West, North, East
+
+    int directionIndex = 0; // index for the direction of the lane where North = 0, East = 1, South = 2, West = 3
+    int greenLightOnTime = 0; // The time the green light is on for the lane in seconds
+    long priorityArray[] = {0, 3, 4, 2}; // priority array for the lanes where indexes are North, East, South, West
+
+    switch ((int) priorityArray[directionIndex]) {
+      case 0:
+        greenLightOnTime = 5;
+        break;
+      case 1:
+        greenLightOnTime = 10;
+        break;
+      case 2:
+        greenLightOnTime = 20;
+        break;
+      case 3:
+        greenLightOnTime = 30;
+        break;
+      case 4:
+        greenLightOnTime = 45;
+        break;
+    }
+
+    long trafficLightCycle = 0; // The total duration of the traffic light cycle in seconds
+
+    // Calculate the total duration of the traffic light cycle as well as converting priorities into times
+    for (int i = 0; i < priorityArray.length; i++) {
+      switch ((int) priorityArray[i]) {
+        case 0:
+          priorityArray[i] = 5L * 1_000_000_000 / 240; // Convert to nanoseconds and simulation time, need to do long calculations to avoid overflow
+          break;
+        case 1:
+          priorityArray[i] = 10L * 1_000_000_000 / 240;
+          break;
+        case 2:
+          priorityArray[i] = 20L * 1_000_000_000 / 240;
+          break;
+        case 3:
+          priorityArray[i] = 30L * 1_000_000_000 / 240;
+          break;
+        case 4:
+          priorityArray[i] = 45L * 1_000_000_000 / 240;
+          break;
+      }
+      System.out.println("Priority for direction " + i + " is " + priorityArray[i] + " nanoseconds");
+      trafficLightCycle += priorityArray[i];
+    }
+
+    System.out.println("Total traffic cycle = " + trafficLightCycle + " nanoseconds");
+
+    final long trafficLightCycleFinal = trafficLightCycle; // final variable for the traffic light cycle
+    final int greenLightOnTimeFinal = greenLightOnTime; // final variable for the green light on time
+
     int simulationTime = 1; // simulation time in hours (15 seconds real time per hour)
 
     Queue<Long>[] outboundCars = new Queue[3];
@@ -95,8 +158,11 @@ class JunctionSimulationLogic {
 
             long threadStartTime = System.nanoTime();
 
-            // Schedule 40 cars to exit over 75 milliseconds
-            for (int i = 0; i < 40; i++) {
+            // assuming 2 cars leave per second
+            int carsLeaving = greenLightOnTimeFinal * 2;
+
+            // Schedule 40 cars to exit over {the priority time} seconds
+            for (int i = 0; i < carsLeaving; i++) {
               final int carIndex = i;
               carsExiting.schedule(
                   () -> {
@@ -104,14 +170,14 @@ class JunctionSimulationLogic {
                     exitQueue(outboundCars[1].poll());
                     exitQueue(outboundCars[2].poll());
                   },
-                  (long) (carIndex * (75.0 / 40)),
-                  TimeUnit.MILLISECONDS); // spread car exits over 75 ms
+                  (long) (carIndex * (priorityArray[directionIndex] / carsLeaving)),
+                  TimeUnit.NANOSECONDS); // spread car exits over the green light's duration in nanoseconds stored in priorityArray[directionIndex]
             }
             
             long actualGreenLightTime = System.nanoTime() - threadStartTime;
 
             // reschedule the next greenlight after 300 milliseconds but also take into account the traffic light green time
-            carsExiting.schedule(this, 300000000 - actualGreenLightTime, TimeUnit.NANOSECONDS);
+            carsExiting.schedule(this, trafficLightCycleFinal - actualGreenLightTime, TimeUnit.NANOSECONDS);
           }
         };
 
