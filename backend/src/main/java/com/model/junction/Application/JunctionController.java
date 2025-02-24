8faa477b99a88e3827bf2b1049b453c6f65a853b
JR <179604553+ascomlexicon@users.noreply.github.com>
2025-02-24 00:57:12 +0000

    Remove unnecessary imports

--- backend/src/main/java/com/model/junction/Application/JunctionController.java
+++ backend/src/main/java/com/model/junction/Application/JunctionController.java
@@ -1,7 +1,6 @@
 package com.model.junction.Application;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
