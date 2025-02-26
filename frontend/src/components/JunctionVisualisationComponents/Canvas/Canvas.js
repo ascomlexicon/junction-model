cc753c9f95b62ed8a5832fc0276f9182f28a5313
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2025-02-26 23:53:47 +0000

    Dynamically resize canvas

--- frontend/src/components/JunctionVisualisationComponents/Canvas/Canvas.js
+++ frontend/src/components/JunctionVisualisationComponents/Canvas/Canvas.js
@@ -6,7 +6,7 @@ const Canvas = props => {
   const { draw, ...rest } = props
   const canvasRef = useCanvas(draw)
   
-  return <canvas ref={canvasRef} {...rest}/>
+  return <canvas ref={canvasRef} {...rest} style={{ width: '100%', height: '100%' }}/>
 }
 
 export default Canvas
