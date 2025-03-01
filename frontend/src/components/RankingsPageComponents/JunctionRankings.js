472ff1046929023dc24e83d28c7cc50c9a24995a
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2025-03-01 13:44:44 +0000

    Add comments indicating all areas for post/get requests

--- frontend/src/components/RankingsPageComponents/JunctionRankings.js
+++ frontend/src/components/RankingsPageComponents/JunctionRankings.js
@@ -7,16 +7,45 @@ import VPHDisplayForm from './VPHDisplayForm';
 import axios from 'axios';
 
 // TODO: When retrieving the ranking for the junction for the first time, it needs to be the same junction that the user configured
-  // Need to remember this somehow so we make the correct request
+  // Need to remember this somehow so we make the correct request - FIXME: SEE NOTES ON MACBOOK/ICLOUD
 const JunctionRankings = () => {
   // Keep track of both selected junction and junctions state
-  const [selectedJunction, setSelectedJunction] = useState(null);
+  // const [selectedJunction, setSelectedJunction] = useState(null);
+
+  const [selectedJunction, setSelectedJunction] = useState(() => {
+    // TODO: Make get request for the default junction passed 
+    // axios.get('___')
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   });
+      // TODO: Optionally add a finally clause too
+  });
+
   const [junctions, setJunctions] = useState([
-    { name: 'Junction 2', score: 79, highlight: false },
-    { name: 'Junction 1', score: 75, highlight: false },
-    { name: 'Bus Lane Junction', score: 70, highlight: false },
-    { name: 'Pedestrian Crossing Junction', score: 57, highlight: false },
-    { name: 'Junction 3', score: 23, highlight: false }
+    // { name: 'Junction 2', score: 79, highlight: false },
+    // { name: 'Junction 1', score: 75, highlight: false },
+    // { name: 'Bus Lane Junction', score: 70, highlight: false },
+    // { name: 'Pedestrian Crossing Junction', score: 57, highlight: false },
+    // { name: 'Junction 3', score: 23, highlight: false }
+
+    
+    // TODO: make a get request which gets all of the junctions in use (ie fetch the project for the junction that was just configured) 
+    // axios.get('___')
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   });
+    // TODO: Optionally add a finally clause too
+
   ]);
 
   const handleSelect = (selectedJunction) => {
@@ -26,6 +55,7 @@ const JunctionRankings = () => {
       highlight: junction.name === selectedJunction.name
     }));
     
+    // TODO: Add a getrequest here, see above
     setJunctions(updatedJunctions);
     setSelectedJunction(selectedJunction);
   };
@@ -33,6 +63,7 @@ const JunctionRankings = () => {
   return (
     <div className={styles.container}>
       <div className = {styles.header}>
+        {/* TODO: This needs to be the project that we are working with */}
         <h1>Named Junction!</h1>
       </div>
       <div className={styles.backButtonContainer}>
