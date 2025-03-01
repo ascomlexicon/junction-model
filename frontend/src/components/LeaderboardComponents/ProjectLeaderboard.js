3049730b33748f1e1d05da92bc3b5064dfd86199
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2025-03-01 18:10:48 +0000

    Add commented code ready for endpoints
    Axios get requests used to get all of the projects stored in the backend, as well as information about a single project (ie the project that is clicked on by the user)

--- frontend/src/components/LeaderboardComponents/ProjectLeaderboard.js
+++ frontend/src/components/LeaderboardComponents/ProjectLeaderboard.js
@@ -4,77 +4,44 @@ import { Link } from "react-router-dom";
 import VPHDataDisplay from './VPHDisplayData'; // Import the VPH data component
 
 const ProjectLeaderboard = () => {
-  // Sample project data with VPH information
-  const [projects, setProjects] = useState([
-    // { 
-    //   name: 'Coventry A', 
-    //   id: 'coventry-a',
-    //   vphData: {
-    //     north: { entering: "1250", exitEast: "320", exitSouth: "530", exitWest: "400" },
-    //     south: { entering: "1180", exitNorth: "510", exitEast: "290", exitWest: "380" },
-    //     east: { entering: "980", exitNorth: "310", exitSouth: "290", exitWest: "380" },
-    //     west: { entering: "1050", exitNorth: "420", exitEast: "310", exitSouth: "320" }
-    //   }
-    // },
-    // { 
-    //   name: 'Warwick', 
-    //   id: 'warwick',
-    //   vphData: {
-    //     north: { entering: "890", exitEast: "280", exitSouth: "410", exitWest: "200" },
-    //     south: { entering: "920", exitNorth: "380", exitEast: "260", exitWest: "280" },
-    //     east: { entering: "750", exitNorth: "250", exitSouth: "210", exitWest: "290" },
-    //     west: { entering: "820", exitNorth: "310", exitEast: "230", exitSouth: "280" }
-    //   }
-    // },
-    // { 
-    //   name: 'Leamington North', 
-    //   id: 'leamington-north',
-    //   vphData: {
-    //     north: { entering: "780", exitEast: "230", exitSouth: "350", exitWest: "200" },
-    //     south: { entering: "820", exitNorth: "310", exitEast: "240", exitWest: "270" },
-    //     east: { entering: "690", exitNorth: "210", exitSouth: "230", exitWest: "250" },
-    //     west: { entering: "750", exitNorth: "280", exitEast: "190", exitSouth: "280" }
-    //   }
-    // },
-    // { 
-    //   name: 'Leamington South', 
-    //   id: 'leamington-south',
-    //   vphData: {
-    //     north: { entering: "830", exitEast: "250", exitSouth: "380", exitWest: "200" },
-    //     south: { entering: "860", exitNorth: "320", exitEast: "260", exitWest: "280" },
-    //     east: { entering: "720", exitNorth: "240", exitSouth: "210", exitWest: "270" },
-    //     west: { entering: "790", exitNorth: "300", exitEast: "220", exitSouth: "270" }
-    //   }
-    // },
-    // { 
-    //   name: 'Kings Cross', 
-    //   id: 'kings-cross',
-    //   vphData: {
-    //     north: { entering: "1520", exitEast: "490", exitSouth: "620", exitWest: "410" },
-    //     south: { entering: "1430", exitNorth: "580", exitEast: "410", exitWest: "440" },
-    //     east: { entering: "1290", exitNorth: "430", exitSouth: "380", exitWest: "480" },
-    //     west: { entering: "1350", exitNorth: "520", exitEast: "390", exitSouth: "440" }
-    //   }
-    // }
-    
-    // TODO: Make get request for ALL projects
-        
-    // axios.get('___')
+  // Used whilst the data is being served from the backend
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(null);
+
+  // All of the projects stored in the backend
+  const [projects, setProjects] = useState([]);
+
+  // Project clicked on by the user, initially set to null
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  useEffect(() => {
+    // This code runs once when the component mounts
+    // axios.get('your_api_endpoint/projects')
     //   .then(function (response) {
     //     // handle success
-    //     console.log(response);
+    //     setProjects(response.data);
+    //     setLoading(false);
     //   })
     //   .catch(function (error) {
     //     // handle error
     //     console.log(error);
+    //     setError(error);
+    //     setLoading(false);
     //   });
-  ]);
-
-  const [selectedProject, setSelectedProject] = useState(null);
+  }, []);
 
   const handleSelectProject = (project) => {
     // TODO: Make get request for the project passed through
-    setSelectedProject(project);
+    // axios.get('___')
+    //   .then(function (response) {
+    //     setSelectedProject(response.data);
+    //   }) 
+    //   .catch(function (error) {
+    //     // Handle error
+    //   });
+
+    // setSelectedProject(project);
   };
   
   return (
