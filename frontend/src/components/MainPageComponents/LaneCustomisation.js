3fb0dd30d2b40551480860ea5459a2cf0f3672ed
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2025-03-03 15:06:48 +0000

    comment

--- frontend/src/components/MainPageComponents/LaneCustomisation.js
+++ frontend/src/components/MainPageComponents/LaneCustomisation.js
@@ -390,6 +390,8 @@ const LaneCustomisation = ({ setActiveStep, saveFormData, resetForm, resetAllFor
   const validateLanes = () => {
     const directions = ['north', 'south', 'east', 'west'];
     let flag = true;
+
+    // FIXME: Issue here, not running, try a different way as in the calculateExitLanes() function?
     directions.array.forEach(dir => {
       let res = calculateExitLanes(dir);
       // Check that res <= the value entered by the user; return false if not
