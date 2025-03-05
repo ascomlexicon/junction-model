0dd20362eea6e6e1aff5b0002cca12a246084541
JR <179604553+ascomlexicon@users.noreply.github.com>
2025-03-05 12:29:51 +0000

    Remove unnecessary constructor

--- backend/src/main/java/com/model/junction/JunctionClasses/JunctionQuarter.java
+++ backend/src/main/java/com/model/junction/JunctionClasses/JunctionQuarter.java
@@ -14,34 +14,5 @@ public record JunctionQuarter(
   boolean hasCrossings,
   int crossingDuration,
   int crossingRequestsPerHour,
-  Double[] score
-) {
-  public JunctionQuarter(
-    Direction outboundDirection,
-    boolean hasLeftTurnLane,
-    int lanesEntering,
-    int lanesExiting,
-    String hasBusCycleLane,
-    int specialVPH,
-    boolean hasPriorities,
-    Direction[] directionOrder,
-    boolean hasCrossings,
-    int crossingDuration,
-    int crossingRequestsPerHour
-  ) {
-    this(
-      outboundDirection,
-      hasLeftTurnLane,
-      lanesEntering, 
-      lanesExiting,
-      hasBusCycleLane,
-      specialVPH,
-      hasPriorities,
-      directionOrder,
-      hasCrossings,
-      crossingDuration,
-      crossingRequestsPerHour,
-      null
-    );
-  }
-}
+  double[] score
+) {}
