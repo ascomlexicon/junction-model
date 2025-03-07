54b9d95ba929781e1398f866d6f530be04afbac0
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2025-03-07 03:22:24 +0000

    get rid of random data in vphDisplayForm
    some issue currently but code has been refactored at a base level

--- frontend/src/components/RankingsPageComponents/VPHDisplayForm.js
+++ frontend/src/components/RankingsPageComponents/VPHDisplayForm.js
@@ -2,54 +2,25 @@ import React, { useEffect, useState } from 'react';
 import styles from './VPHDisplayForm.module.css';
 import StaticJunctionDisplay from './StaticJunctionDisplay';
 
-// TODO: Need to pass a parameter containing the data to here instead
-function VPHDisplayForm() {
-  const [trafficData, setTrafficData] = useState({
-    north: { south: 0, east: 0, west: 0 },
-    south: { north: 0, east: 0, west: 0 },
-    east: { north: 0, south: 0, west: 0 },
-    west: { north: 0, south: 0, east: 0 }
-  });
-
-  useEffect(() => {
-    const generateRandomData = () => ({
-      north: { south: Math.floor(Math.random() * 151) + 50, east: Math.floor(Math.random() * 151) + 50, west: Math.floor(Math.random() * 151) + 50 },
-      south: { north: Math.floor(Math.random() * 151) + 50, east: Math.floor(Math.random() * 151) + 50, west: Math.floor(Math.random() * 151) + 50 },
-      east: { north: Math.floor(Math.random() * 151) + 50, south: Math.floor(Math.random() * 151) + 50, west: Math.floor(Math.random() * 151) + 50 },
-      west: { north: Math.floor(Math.random() * 151) + 50, south: Math.floor(Math.random() * 151) + 50, east: Math.floor(Math.random() * 151) + 50 }
-    });
-
-    const savedData = localStorage.getItem('trafficData');
-    if (savedData) {
-      try {
-        setTrafficData(JSON.parse(savedData));
-      } catch (e) {
-        console.error('Error parsing saved traffic data:', e);
-        setTrafficData(generateRandomData());
-      }
-    } else {
-      setTrafficData(generateRandomData());
-    }
-  }, []);
-
+function VPHDisplayForm({ vphData = {} }) {
   return (
     <div className={styles.formsGrid}>
       <h2>VPH Data for this Project</h2>
       <div className={styles.formCell}>
         <h1>North</h1>
-        <StaticJunctionDisplay outgoingDirection1="South" outgoingDirection2="East" outgoingDirection3="West" values={trafficData.north} />
+        <StaticJunctionDisplay outgoingDirection1="South" outgoingDirection2="East" outgoingDirection3="West" values={vphData.vphNorth} />
       </div>
       <div className={styles.formCell}>
         <h1>South</h1>
-        <StaticJunctionDisplay outgoingDirection1="North" outgoingDirection2="East" outgoingDirection3="West" values={trafficData.south} />
+        <StaticJunctionDisplay outgoingDirection1="North" outgoingDirection2="East" outgoingDirection3="West" values={vphData.vphSouth} />
       </div>
       <div className={styles.formCell}>
         <h1>East</h1>
-        <StaticJunctionDisplay outgoingDirection1="North" outgoingDirection2="South" outgoingDirection3="West" values={trafficData.east} />
+        <StaticJunctionDisplay outgoingDirection1="North" outgoingDirection2="South" outgoingDirection3="West" values={vphData.vphEast} />
       </div>
       <div className={styles.formCell}>
         <h1>West</h1>
-        <StaticJunctionDisplay outgoingDirection1="North" outgoingDirection2="South" outgoingDirection3="East" values={trafficData.west} />
+        <StaticJunctionDisplay outgoingDirection1="North" outgoingDirection2="South" outgoingDirection3="East" values={vphData.vphWest} />
       </div>
     </div>
   );
